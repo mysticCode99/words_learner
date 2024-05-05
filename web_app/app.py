bba668de546ac17ec18678b1d9ba42ee2ff358b1
mysticCode99 <arturmargaryan999@gmail.com>
2024-05-05 20:23:45 +0400

    initial commit

---
+++ web_app/app.py
@@ -0,0 +1,25 @@
+
+
+import os
+from flask import Flask, render_template
+
+from singltone import Config_Provider
+
+# CONFIG_PROVIDER = Singltone()
+# MODERATOR = Moderator()
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title_name='Home')
+
+if __name__ == '__main__':
+    # set_navbar_links()
+    # chrome://net-internals/#sockets
+    app.config['SECRET_KEY'] = Config_Provider.SECRET_KEY
+    app.config.update(
+        dict(DATABASE=os.path.join(app.root_path, Config_Provider.DATABASE_PATH))
+    )
+    app.run(port=8000, debug=Config_Provider.DEBUG)
---
+++ web_app/singltone.py
@@ -0,0 +1,6 @@
+
+
+class Config_Provider():
+    DATABASE_PATH = '/tmp/flsite.db'
+    DEBUG = True
+    SECRET_KEY = 'sdfvresdbvrscrw85w7rsfv68w4rv'
