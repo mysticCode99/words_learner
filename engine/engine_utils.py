b8c2693dda9c773631ac4f6094e6ec3aca6d054e
mysticCode99 <arturmargaryan999@gmail.com>
2024-05-12 23:03:31 +0400

    Task: Added words repeating part

--- engine/engine_utils.py
+++ engine/engine_utils.py
@@ -1,4 +1,6 @@
 
+from time import time
+
 class Library():
     def __init__(self) -> None:
         pass
@@ -8,8 +10,19 @@ class Collection():
         pass
 
 class Word():
-    def __init__(self, word:str, translation:str) -> None:
-        self.word = ''
-        self.translation = ''
+    def __init__(self, word:str, translation:str, l_time:int=time()) -> None:
+        self.word = word
+        self.translation = translation
+        self.last_time = l_time
         self.repeated = 0
         self.correct = 0
+    
+    def check_word(self, word:str) -> bool:
+        """sumary_line"""
+        self.repeated += 1
+        self.last_time = time()
+        if word == self.word:
+            self.correct += 1
+            return True
+        else:
+            return False
--- engine/words_memorise.py
+++ engine/words_memorise.py
@@ -1,18 +1,74 @@
 
-from engine_utils import Word
+import os, random
+from datetime import datetime
 
-WORDS = []
+from engine_utils import Word
+from engine_singltone import Engine_Singltone
+
+def choose_lang():
+    """sumary_line"""
+    lang = input("Choose Language > ").strip()
+    Engine_Singltone.set_lang(lang)
 
 def add_word():
+    """ Add word """
+    if not Engine_Singltone.get_current_lang():
+        choose_lang()
+    word = input("New word > ").strip()
+    translation = input("Translation > ").strip()
+    Engine_Singltone.add_word(Word(word, translation))
+
+def add_from_file():
+    """ Adding words from file """
+    if not Engine_Singltone.get_current_lang():
+        choose_lang()
+    fp = input("Enter file Path > ").strip()
+    if not os.path.exists(fp):
+        print(f"WARNING: {fp} not found")
+        return
+    with open(fp, 'r') as f:
+        for line in f:
+            word, translation = line.split(',')[:2]
+            Engine_Singltone.add_word(Word(
+                word.strip(),
+                translation.strip()
+            ))
+
+def show_words():
     """sumary_line"""
-    word = input("New word >")
-    translation = input("Translation >")
-    WORDS.append(Word(word, translation))
-    return None
+    for w in Engine_Singltone.get_last_ten_words():
+        date_obj = datetime.fromtimestamp(w.last_time)
+        print(f"{w.word:>30} <-> {w.translation:>30} : {date_obj.strftime(r'%d:%m:%y %H:%M')}")
+    
+def repeat_words():
+    """sumary_line"""
+    w_list = Engine_Singltone.get_words()
+    try:
+        count = int(input("Enter count of words > "))
+    except ValueError:
+        print("Error: input need to be integer")
+        return
+    while count > 0:
+        count -= 1
+        w = random.choice(w_list)
+        print(f"Translation -> {w.translation}")
+        answer = input("Word -> ")
+        if w.check_word(answer):
+            print('Correct')
+        else:
+            print('Uncorrect !!!')
+            print(f"Correct answer is -> {w.word}")
+        print('-'*100)
+        
 
 def choose_action():
-    inp = input('chose work mode >')
-    action = actions.get(inp)
+    """ Choose ation """
+    actions = list(ACTIONS.keys())
+    print(' | '.join(actions))
+    inp = input("Choose work mode > ").strip()
+    action = ACTIONS.get(inp)
+    if not action:
+        print("ERROR: Action not found")
     return action
 
 def main():
@@ -24,10 +80,14 @@ def main():
         else:
             action = action()
 
-actions = {
-    'add_word'  : add_word,
-    'exit'      : exit,
-    'change'    : choose_action,
+ACTIONS = {
+    'add_word'          : add_word,
+    'exit'              : exit,
+    'change'            : choose_action,
+    'choose_laguage'    : choose_lang,
+    'add_from_file'     : add_from_file,
+    'show_words'        : show_words,
+    'repeat_words'      : repeat_words,
 }
 
 if __name__ == '__main__':
